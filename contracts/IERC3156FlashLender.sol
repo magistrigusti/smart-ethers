e9a1cf19d796f7bedbfcd540df99318a4d115f64
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-07-09 13:04:55 +0300

    0.1.4

---
+++ contracts/IERC3156FlashLender.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+import {IERC3156FlashBorrower} from "./IERC3156FlashBorrower.sol";
+
+interface IERC3156FlashLender {
+    function flashFee(address token, uint256 amount) external view returns (uint256);
+
+    function flashLoan(
+        IERC3156FlashBorrower receiver,
+        address token,
+        uint256 amount,
+        bytes calldata data
+    ) external returns (bool);
+}
