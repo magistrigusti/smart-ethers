bc90b0ae1182c31917a6e0c962c0a8f9afe16067
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-07-13 19:19:59 +0300

    0.2.8

--- tasks/sample.ts
+++ tasks/sample.ts
@@ -15,9 +15,9 @@ task("get_owner", "Read Demo owner")
 
 task("call_run", "Calls run() on Demo")
   .addParam("contract", "Contract to call")
-  .addParam("value", "Amount to pay")
-  .addParam("a", "Value of a")
-  .addOptionalParam("signer", "Signer index", 0, types.)
+  .addParam("value", "Amount to pay", 0, types.int)
+  .addParam("a", "Value of a", 0, types.int)
+  .addOptionalParam("signer", "Signer index", 0, types.int)
   .setAction(async (taskArgs, { ethers }) => {
     const signer = (await ethers.getSigners())[taskArgs.signer];
     console.log(`Using signer ${signer.address}`);
@@ -29,5 +29,18 @@ task("call_run", "Calls run() on Demo")
       `Demo balance: ${await ethers.provider.getBalance(demo.target)}`,
     );
     console.log(`Value of a: ${await demo.a()}`);
+  });
+
+task("get_balances", "Reads balances from multiple addrs")
+  .addParam("addresses", "Addresses to read balances from")
+  .setAction(async(taskArgs, {ethers}) => {
+    console.log(taskArgs.addresses);
+    console.log(typeof taskArgs.addresses);
+    const addrs = taskArgs.addresses.split(",");
+    await Promise.all(
+      addrs.map(async (addr: string) => {
+        console.log(await ethers.provider.getBalance(addr));
+      })
+    )
   });
 
